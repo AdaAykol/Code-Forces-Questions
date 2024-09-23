7b0003fa87159b84f147ee8d7ce749b32ce2c0b9
Ada Aykol <ada@credil.org>
2024-09-23 13:15:50 +0300

    solution to the Plus-or-mines question

---
+++ Plus-or-minus.rb
@@ -0,0 +1,24 @@
+input = gets.to_i
+
+array=[]
+
+input.times do
+  
+  n = gets
+
+  n = n.split.map {|element| element.to_i}
+  
+  a = n[0]
+  b = n[1]
+  c = n[2]
+
+  if(a+b == c)
+    array.push("+")
+  else
+    array.push("-")
+  end
+end
+
+for i in 0..array.length-1
+  puts array[i]
+end
