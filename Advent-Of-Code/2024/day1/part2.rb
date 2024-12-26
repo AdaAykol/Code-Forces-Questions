4bef37098b555eb59881898be586a6b185aa0067
Ada Aykol <ada-aykol@hotmail.com>
2024-12-26 17:23:40 +0300

    improved the solution for part2 question

--- Advent-Of-Code/2024/day1/part2.rb
+++ Advent-Of-Code/2024/day1/part2.rb
@@ -8,13 +8,10 @@ input.each_line do |line|
   list1 << numbers[0].to_i
   list2 << numbers[-1].to_i
 end
-count=0
+
 list1.each do |number|
-  list2.each do |list_number|
-    count = count+1 if number==list_number
-  end
-  
-  answer = answer + (number * count)
-  count=0
+  count=list2.count(number)
+  answer += (number * count)
 end
+
 puts answer
