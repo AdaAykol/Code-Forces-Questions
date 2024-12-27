13685e4da3f61b60b03bc605c90c25ceb4d12b49
Ada Aykol <ada-aykol@hotmail.com>
2024-12-27 21:05:44 +0300

    WIP i got stuck

--- Advent-Of-Code/day2/part2.rb
+++ Advent-Of-Code/day2/part2.rb
@@ -1,54 +1,166 @@
+# # THE VERSION I DID NOT WORKING 
+
+# def detect_discrepancy(list)
+#   previous_direction = nil
+
+#   list.each_cons(2) do |current, incoming|
+#     if incoming > current
+#       if previous_direction == :descending
+#         @count = @count + 1
+#         return true 
+#       end
+#       previous_direction = :ascending
+#     elsif current > incoming
+#       if previous_direction == :ascending
+#         @count = @count + 1
+#         return true
+#       end
+#       previous_direction = :descending
+#     end
+#   end
+
+#   puts "this is counter test #{@count}"
+#   return false
+# end
+
+# def main
+#   input = $stdin.read
+#   safe = true
+#   @answer = 0
+#   @count = 0
+#   input.each_line do |line|
+#     list = line.split.map(&:to_i)
+
+
+#     if detect_discrepancy(list)
+#       safe = false
+#     else
+#       list.each_with_index do |number, i|
+#         if i > 0 && i + 1 < list.length       # I HAD ISSUES FINDING OUT THIS IF STATEMENT TO PREVENT OVERBOUNDING 
+#           if (number - list[i + 1]).abs > 3 
+#             safe = false
+#             @count = 10
+#           end
+#           if number == list[i + 1] && (list[i+1] - list[i-1]).abs >3
+#             safe = false
+#             @count = @count + 1
+#           end
+#         end
+#       end
+#     end
+
+
+#     puts "the unsafe counter for the #{list} list: #{@count}"
+
+
+#     if @count <= 1
+#       safe = true
+#     else
+#       puts "contains more than one errors"
+#     end
+
+#     if safe
+#       @answer = @answer + 1
+#       puts "This list is safe"
+#     else
+#       puts "this list is UNSAFE"
+#     end
+
+
+#     @count=0
+#     safe = true
+#   end
+
+# end
+
+
+# main
+# puts "The answer is: #{@answer}"
+
+
+## VERSION I DID WITH HELP (I ONLY GOT HELP AFTER 1:30 HOURS AND EVEN THOUGH I THOUGHT I HAD CORRECT ANSWERS I DID NOT)
+
+
+
 def detect_discrepancy(list)
   previous_direction = nil
 
   list.each_cons(2) do |current, incoming|
     if incoming > current
-      return true if previous_direction == :descending
+      if previous_direction == :descending
+        @count = @count + 1
+        return true 
+      end
       previous_direction = :ascending
     elsif current > incoming
-      return true if previous_direction == :ascending
+      if previous_direction == :ascending
+        @count = @count + 1
+        return true
+      end
       previous_direction = :descending
     end
   end
 
-  false
+  puts "this is counter test #{@count}"
+  return false
 end
 
-input = $stdin.read
-safe = true
-answer = 0
 
-input.each_line do |line|
-  list = line.split.map(&:to_i)
+def is_list_safe_with_dampener?(list)
 
+  if !safet_checker?(list)
+    
+  end
 
-  if detect_discrepancy(list)
-    safe = false
-  else
-    list.each_with_index do |number, i|
-      if i + 1 < list.length 
-
-        # puts "This is the line: #{list}"
-
-        if (number - list[i + 1]).abs > 3 || number == list[i + 1]
-          safe = false
-        end
-
-        # puts "This is the number: #{number}"
-        # puts "This is the next number: #{list[i + 1]}"
-        # puts "The difference is: #{(number - list[i + 1]).abs}"
-      end
+  list.each_with_index do |_, i|
+    modified_list = list.dup
+    modified_list.delete_at(i)
+    puts modified_list.inspect
+    if !detect_discrepancy(modified_list)
+      return true 
     end
   end
 
-  if safe
-    answer += 1
-    # puts "Increased the answer: #{answer}"
-  end
-
-  safe = true
+  return false
 end
 
-puts "The answer is: #{answer}"
+def safety_checker?(list)
+  list.each_with_index do |number, i|
+    if i > 0 && i + 1 < list.length       # I HAD ISSUES FINDING OUT THIS IF STATEMENT TO PREVENT OVERBOUNDING 
+      if (number - list[i + 1]).abs > 3 
+        @safe = false
+        @count = 10
+      end
+      if number == list[i + 1] && (list[i+1] - list[i-1]).abs >3
+        @safe = false
+        @count = @count + 1
+      end
+    end
+  end
+end
 
-# i had a hard time with the method `detect_disperancy`
+
+def main
+  input = $stdin.read
+  @safe = true
+  @answer = 0
+  @count = 0
+  input.each_line do |line|
+    list = line.split.map(&:to_i)
+
+    if detect_discrepancy(list)
+      @safe = false
+    end
+    
+    if is_list_safe_with_dampener?(list)
+      
+    end
+
+
+
+  end
+
+end
+
+main
+puts "The answer is: #{@answer}"
