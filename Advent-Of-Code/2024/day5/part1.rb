33043b3465a968d206def3e8fe59057bbc1985ad
Ada Aykol <ada-aykol@hotmail.com>
2025-01-06 16:54:02 +0300

    solved day5 part1

--- Advent-Of-Code/2024/day5/part1.rb
+++ Advent-Of-Code/2024/day5/part1.rb
@@ -3,20 +3,42 @@ before_empty = true
 
 rules = []
 arr2 = []
+
 input.each_line do |line|
   line = line.split
 
   if line.empty?
     before_empty = false
   end
-
   if before_empty
-    rules << line
-  else
-    arr2 << line
+    first, second = line[0].split("|")
+    rules << [first.to_i, second.to_i]
+  elsif line != []
+
+    number_set = line[0].split(",").map(&:to_i)
+    arr2 << number_set
   end
 end
 
-
 puts "This is the rules list: #{rules}"
 puts "this is the arr2: #{arr2}"
+
+answer=0
+safe = true
+arr2.each do |arr|
+  rules.each do |rule|
+    if arr.include?(rule[0]) && arr.include?(rule[1])
+      first_index = arr.index(rule[0])
+      second_index = arr.index(rule[1])
+      if first_index > second_index 
+        safe = false
+      end
+    end
+  end
+
+  if safe
+    answer = answer+arr[arr.length/2]
+  end
+end
+
+puts answer
